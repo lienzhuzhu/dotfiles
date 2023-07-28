d283d0314ac14ca908133cb017e85a140dfb30a5
Lien Zhu <lienzhucs@gmail.com>
2023-07-27 22:46:15 -0500

    finally have a nice vim config

---
+++ nvim/init.lua
@@ -0,0 +1,13 @@
+require('plugins')
+
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.o.relativenumber = true
+vim.o.number = true
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,21 @@
+return require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+
+    -- colorscheme
+    use 'gruvbox-community/gruvbox'
+    use 'sainnhe/everforest'
+
+    -- telescope fuzzy finder
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    -- treesitter syntax highlighting
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        {run = ':TSUpdate'}
+    }
+
+end)
