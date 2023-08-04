9f7c49c83dbc6153c2b0c278503270c999aec666
Lien Zhu <lienzhucs@gmail.com>
2023-08-03 19:58:43 -0700

    created links to alacritty and starship.toml

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/bash
 
+# MUST INSTALL:
+# - Rust
+# - a Nerd Font
+# - Neovim
+# - ripgrep
+
+
+# alacritty configs
+rm -rf ~/.config/alacritty
+ln -s $(pwd)/alacritty ~/.config/alacritty
+
+# starship prompt config
+rm -f ~/.config/starship.toml
+ln -s $(pwd)/starship.toml ~/.config/starship.toml
+
 # nvim configs
-# install packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-rm -rf ~/.config/nvim
-ln -s $(pwd)/nvim ~/.config/nvim
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim         # install packer
+rm -rf ~/.config/nvim                                           # cleans out any existing nvim
+ln -s $(pwd)/nvim ~/.config/nvim                                # create link
