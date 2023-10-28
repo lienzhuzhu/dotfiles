a5a526a5e24d97d8dc3e4006c162ff869fceede1
Lien Zhu <lienzhucs@gmail.com>
2023-10-27 17:14:33 -0700

    disable mouse

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,3 +11,11 @@ vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+
+vim.o.wrap = false
+
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+vim.o.scrolloff = 8
+vim.o.mouse = ""
