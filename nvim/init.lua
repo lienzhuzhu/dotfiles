4e811e56c8dfc4a6ac4a1cc69f9ace6fde141be0
Lien Zhu <lienzhucs@gmail.com>
2023-12-15 05:59:00 -0800

    separated remaps and settings for neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,3 @@
 require('plugins')
-
-vim.g.mapleader = " "
-
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
-vim.o.relativenumber = true
-vim.o.number = true
-
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-
-vim.o.wrap = false
-
-vim.o.hlsearch = false
-vim.o.incsearch = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
-vim.o.scrolloff = 8
-
-vim.o.laststatus = 2
---vim.o.showtabline = 2
+require('remap')
+require('set')
