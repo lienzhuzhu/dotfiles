f8767d30a575fe67e06516f3476eb6d1ff22f74c
Lien Zhu <lienzhucs@gmail.com>
2023-12-15 05:52:25 -0800

    starting to separate maps and settings in neovim

---
+++ nvim/lua/set.lua
@@ -0,0 +1,19 @@
+vim.o.relativenumber = true
+vim.o.number = true
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.wrap = false
+
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.scrolloff = 8
+
+vim.o.laststatus = 2
+--vim.o.showtabline = 2
