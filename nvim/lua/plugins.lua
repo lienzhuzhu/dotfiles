a33f5a733cc251642e9f7b3188e7f125ffcdd468
Lien Zhu <lienzhucs@gmail.com>
2023-12-15 18:56:10 -0800

    adds lualine to nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -20,10 +20,15 @@ return require('packer').startup(function()
         branch = "v3.x",
         requires = {
           "nvim-lua/plenary.nvim",
-          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+          "nvim-tree/nvim-web-devicons",
           "MunifTanjim/nui.nvim",
-          -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+          -- "3rd/image.nvim", -- image preview support
         }
+    }
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
 
 end)
