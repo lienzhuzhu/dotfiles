9988e02c74fa5573f98fb012d696c785c77fc0e4
Lien Zhu <lienzhucs@gmail.com>
2023-12-15 19:28:56 -0800

    bootstraps packer for neovim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,22 @@
-return require('packer').startup(function()
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+
+local packer_bootstrap = ensure_packer()
+
+
+return require('packer').startup(function(use)
 
     use 'wbthomason/packer.nvim'
+
     use 'EdenEast/nightfox.nvim'
 
     use {
@@ -19,10 +35,10 @@ return require('packer').startup(function()
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
         requires = {
-          "nvim-lua/plenary.nvim",
-          "nvim-tree/nvim-web-devicons",
-          "MunifTanjim/nui.nvim",
-          -- "3rd/image.nvim", -- image preview support
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- image preview support
         }
     }
 
@@ -30,5 +46,10 @@ return require('packer').startup(function()
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 
 end)
