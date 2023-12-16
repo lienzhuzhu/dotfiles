17631eb6da36c19d1c33d8dda24ea8eb6fe441f5
Lien Zhu <lienzhucs@gmail.com>
2023-12-16 00:59:57 -0800

    still not sure why neo-tree configs have to be in after

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,5 +2,4 @@ return {
 
      "christoomey/vim-tmux-navigator",
 
-
 }
--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -11,14 +11,6 @@ return {
     config = function()
 
         vim.keymap.set("n", "<leader>t", ":Neotree toggle<CR>")
-        require("neo-tree").setup({
-            close_if_last_window = true,
-            window = {
-                mappings = {
-                    ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
-                }
-            }
-        })
 
     end
 }
