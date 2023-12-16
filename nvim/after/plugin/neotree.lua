d86731d4f7204c25ce5291c7cd8c10d112bf75be
Lien Zhu <lienzhucs@gmail.com>
2023-12-15 20:27:53 -0800

    refines some settings with file preview in neotree

--- nvim/after/plugin/neotree.lua
+++ nvim/after/plugin/neotree.lua
@@ -1,7 +1,12 @@
 require("neo-tree").setup({
     close_if_last_window = true,
     filesystem = {
-        hijack_netrw_behavior = "open_default"
+        hijack_netrw_behavior = "open_current"
+    },
+    window = {
+        mappings = {
+            ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
+        }
     }
 })
 
--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,11 +1,9 @@
-require'nvim-treesitter.configs'.setup( 
-    {
-      ensure_installed = { "c", "lua", "python" },
-      sync_install = false,
-      auto_install = false,
-      highlight = {
+require'nvim-treesitter.configs'.setup({
+    ensure_installed = { "c", "lua", "python" },
+    sync_install = false,
+    auto_install = false,
+    highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
-      },
-    }
-)
+    },
+})
