23f2b82af9af9a210fdef83f145807177e4b310f
Lien Zhu <lienzhucs@gmail.com>
2023-12-15 23:24:59 -0800

    figured out the problem is with neo-tree, for some reason its configs must be in after/plugin/neo-tree.lua

--- nvim/after/plugin/colors.lua
+++
@@ -1,6 +0,0 @@
-require('nightfox').setup()
-vim.cmd("colorscheme nordfox")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-vim.opt.termguicolors = true
---
+++ nvim/lua/plugins/colors.lua
@@ -0,0 +1,12 @@
+return {
+    "EdenEast/nightfox.nvim",
+
+    config = function()
+
+        vim.cmd("colorscheme nordfox")
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.opt.termguicolors = true
+
+    end
+}
