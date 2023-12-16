59dd8c61b7d078dae6076065f367a940db0df974
Lien Zhu <lienzhucs@gmail.com>
2023-12-16 00:53:20 -0800

    testing neotree

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,9 +1,24 @@
 return {
-    "EdenEast/nightfox.nvim",
+    --"EdenEast/nightfox.nvim",
+    "rebelot/kanagawa.nvim",
 
     config = function()
 
-        vim.cmd("colorscheme nordfox")
+        require('kanagawa').setup({
+            compile = false,             -- enable compiling the colorscheme
+            undercurl = true,            -- enable undercurls
+            commentStyle = { italic = true },
+            background = {               -- map the value of 'background' option to a theme
+                dark = "dragon",           -- try "dragon" !
+                light = "lotus"
+            },
+        })
+        vim.cmd("colorscheme kanagawa-dragon")
+
+        --require('nightfox').setup()
+        --vim.cmd("colorscheme nordfox")
+
+
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         vim.opt.termguicolors = true
