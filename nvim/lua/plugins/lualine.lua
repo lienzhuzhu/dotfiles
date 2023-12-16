3915ea8401cb251e224d5621cab0183e6f956770
Lien Zhu <lienzhucs@gmail.com>
2023-12-16 00:16:09 -0800

    adds tabline

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,7 +4,16 @@ return {
 
     config = function()
 
-        require('lualine').setup()
+        require('lualine').setup({
+            tabline = {
+                lualine_a = {},
+                lualine_b = {'branch'},
+                lualine_c = {'filename'},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {}
+            }
+        })
 
     end
 }
