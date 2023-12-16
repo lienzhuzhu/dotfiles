5ff175ffbc34af0aa7cab9a57252f54dfecd2c81
Lien Zhu <lienzhucs@gmail.com>
2023-12-15 16:52:21 -0800

    prepping for refactor

--- nvim/plugin/neotree.lua
+++ nvim/plugin/neotree.lua
@@ -1,8 +1,8 @@
-vim.keymap.set("n", "<leader>v", ":Neotree toggle<CR>")
-
 require("neo-tree").setup({
     close_if_last_window = true,
     filesystem = {
         hijack_netrw_behavior = "open_default"
     }
 })
+
+vim.keymap.set("n", "<leader>v", ":Neotree toggle<CR>")
