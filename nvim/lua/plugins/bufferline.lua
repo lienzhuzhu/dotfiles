3fad2b42bbed7a036e1bd9c526224229e12236f7
Lien Zhu <lienzhucs@gmail.com>
2023-12-16 23:42:52 -0800

    actually, i got bufferline to work

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,18 @@
+return {
+    'akinsho/bufferline.nvim', 
+    dependencies = { {'nvim-tree/nvim-web-devicons'} }, 
+
+    config = function()
+
+        require("bufferline").setup({
+            options = {
+                offsets = {
+                    {
+                        filetype = "NvimTree"
+                    }
+                }
+            }
+        })
+
+    end
+}
