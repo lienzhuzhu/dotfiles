e2215b94a2f79744e8b2c3341d3f0792f18acec3
Lien Zhu <lienzhucs@gmail.com>
2023-12-16 21:56:32 -0800

    adds colorizer

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,26 +1,36 @@
 return {
-    --"EdenEast/nightfox.nvim",
-    "rebelot/kanagawa.nvim",
+    {
+        --"EdenEast/nightfox.nvim",
+        "rebelot/kanagawa.nvim",
 
-    config = function()
+        config = function()
 
-        require('kanagawa').setup({
-            compile = false,
-            undercurl = true,
-            commentStyle = { italic = true },
-            background = {
-                dark = "dragon",
-                light = "lotus"
-            },
-        })
-        vim.cmd("colorscheme kanagawa-dragon")
+            require('kanagawa').setup({
+                compile = false,
+                undercurl = true,
+                commentStyle = { italic = true },
+                background = {
+                    dark = "dragon",
+                    light = "lotus"
+                },
+            })
+            vim.cmd("colorscheme kanagawa-dragon")
 
-        --require('nightfox').setup()
-        --vim.cmd("colorscheme nordfox")
+            --require('nightfox').setup()
+            --vim.cmd("colorscheme nordfox")
 
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.opt.termguicolors = true
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-    end
+        end
+    },
+    {
+        "norcalli/nvim-colorizer.lua",
+
+        config = function()
+
+            require'colorizer'.setup()
+
+        end
+    }
 }
--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -20,3 +20,5 @@ vim.o.showtabline = 2
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
