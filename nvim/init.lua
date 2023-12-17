49863a574b9dabe9ed5845eafbbbcef3453baf1f
Lien Zhu <lienzhucs@gmail.com>
2023-12-16 19:39:26 -0800

    removes annoying notification upon changes in plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,5 +15,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local opts = {
+    change_detection = {
+        notify = false
+    }
+}
 
-require('lazy').setup("plugins")
+require('lazy').setup("plugins", opts)
--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -5,11 +5,11 @@ return {
     config = function()
 
         require('kanagawa').setup({
-            compile = false,             -- enable compiling the colorscheme
-            undercurl = true,            -- enable undercurls
+            compile = false,
+            undercurl = true,
             commentStyle = { italic = true },
-            background = {               -- map the value of 'background' option to a theme
-                dark = "dragon",           -- try "dragon" !
+            background = {
+                dark = "dragon",
                 light = "lotus"
             },
         })
@@ -17,7 +17,6 @@ return {
 
         --require('nightfox').setup()
         --vim.cmd("colorscheme nordfox")
-
 
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
