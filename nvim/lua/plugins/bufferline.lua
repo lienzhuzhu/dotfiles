4c8bfb7a693adfd70eaaf9301b41ff1d6fb33cca
Lien Zhu <lienzhucs@gmail.com>
2023-12-17 16:55:21 -0800

    nvim: adds buffer keymaps

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -14,5 +14,8 @@ return {
             }
         })
 
+        vim.keymap.set("n", "<leader>[", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>]", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+
     end
 }
