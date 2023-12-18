4ac7aa15663e20d6ce430b895e2a5a9de04b6201
Lien Zhu <lienzhucs@gmail.com>
2023-12-17 18:05:08 -0800

    nvim: switched keymaps for live_grep and grep_string

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,8 +8,8 @@ return {
 
         vim.keymap.set('n', '<leader>f', builtin.find_files, {})
         vim.keymap.set('n', '<leader>r', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>s', builtin.grep_string, {})
+        vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>g', builtin.grep_string, {})
 
     end
 }
