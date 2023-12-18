e38bad5e3488b8b071587c6595d2bb0188ff506f
Lien Zhu <lienzhucs@gmail.com>
2023-12-18 14:49:16 -0800

    nvim: fixed colorscheme and bufferline race condition

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,21 @@
+return {
+    'akinsho/bufferline.nvim', 
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, 
+
+    config = function()
+
+        require("bufferline").setup({
+            options = {
+                offsets = {
+                    {
+                        filetype = "NvimTree"
+                    }
+                }
+            }
+        })
+
+        vim.keymap.set("n", "_", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "+", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+
+    end
+}
---
+++ nvim/lua/unused/barbar.lua
@@ -0,0 +1,26 @@
+return {
+    'romgrk/barbar.nvim',
+    dependencies = {
+        'lewis6991/gitsigns.nvim',
+        'nvim-tree/nvim-web-devicons',
+    },
+    version = '^1.0.0',
+
+    init = function()
+        vim.g.barbar_auto_setup = false
+    end,
+
+    config  = function()
+
+        require('barbar').setup({
+            animation = false,
+            sidebar_filetypes = {
+                NvimTree = true,
+            }
+        })
+
+        vim.keymap.set("n", "_", ":BufferPrevious<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "+", ":BufferNext<CR>", { noremap = true, silent = true })
+
+    end
+}
