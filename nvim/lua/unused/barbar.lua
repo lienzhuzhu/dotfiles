47d80f272bfd62df3ea7185133bc217f2018545b
Lien Zhu <lienzhucs@gmail.com>
2023-12-18 03:14:45 -0800

    nvim: nevermind, back to bufferlien

--- nvim/lua/unused/barbar.lua
+++ nvim/lua/unused/barbar.lua
@@ -9,6 +9,13 @@ return {
 
         vim.g.barbar_auto_setup = false
 
+        local map = vim.api.nvim_set_keymap
+        local opts = { noremap = true, silent = true }
+
+        -- Move to previous/next
+        map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+        map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+
     end,
 
     opts = {
