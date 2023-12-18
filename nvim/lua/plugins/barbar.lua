561227ec9d05304b0a4ac007e6cc5ceaddf9279c
Lien Zhu <lienzhucs@gmail.com>
2023-12-18 03:20:34 -0800

    nvim: back to barbar I go

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -13,8 +13,8 @@ return {
         local opts = { noremap = true, silent = true }
 
         -- Move to previous/next
-        map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-        map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+        map('n', '_', '<Cmd>BufferPrevious<CR>', opts)
+        map('n', '+', '<Cmd>BufferNext<CR>', opts)
 
     end,
 
