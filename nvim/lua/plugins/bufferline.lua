87fbda4119c3de60f50935f363daf2eedea62630
Lien Zhu <lienzhucs@gmail.com>
2023-12-17 17:52:03 -0800

    nvim: changes buffer nav keymaps

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -14,8 +14,8 @@ return {
             }
         })
 
-        vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
-        vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "_", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "+", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
 
     end
 }
