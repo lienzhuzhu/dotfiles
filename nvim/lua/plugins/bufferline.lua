3ae301dc4ab81847b124d5ee3fe9b00c28406106
Lien Zhu <lienzhucs@gmail.com>
2023-12-17 17:29:38 -0800

    nvim: renames remap.lua to keymaps.lua

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,6 @@
 return {
     'akinsho/bufferline.nvim', 
-    dependencies = { {'nvim-tree/nvim-web-devicons'} }, 
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, 
 
     config = function()
 
@@ -14,8 +14,8 @@ return {
             }
         })
 
-        vim.keymap.set("n", "<leader>[", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
-        vim.keymap.set("n", "<leader>]", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
 
     end
 }
--- nvim/lua/remap.lua
+++
@@ -1,4 +0,0 @@
-vim.g.mapleader = " "
-
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
