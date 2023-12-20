5f91b4d6e329cf17924db536a73b9d6b57b852bb
Lien Zhu <lienzhucs@gmail.com>
2023-12-19 20:12:59 -0800

    adds script to test icons

---
+++ icons.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+printf "%b\n" "Normal"
+printf "%b\n" "\033[1mBold\033[22m"
+printf "%b\n" "\033[3mItalic\033[23m"
+printf "%b\n" "\033[3;1mBold Italic\033[0m"
+printf "%b\n" "\033[4mUnderline\033[24m"
+printf "%b\n" "== === !== >= <= =>"
+printf "%b\n" "󰐊     󰄉      󰑓 󰒲 "
