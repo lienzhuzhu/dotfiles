0ba9d8cd7222733bfa086edb9bf836c6043c3513
Lien Zhu <lienzhucs@gmail.com>
2023-12-20 08:49:44 -0800

    nvim: will need to research the cmp dependencies

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -5,7 +5,7 @@ return {
         "hrsh7th/cmp-buffer", -- source for text in buffer
         "hrsh7th/cmp-path", -- source for file system paths
         "L3MON4D3/LuaSnip", -- snippet engine
-        "saadparwaiz1/cmp_luasnip", -- for autocompletion
+        "saadparwaiz1/cmp_luasnip", -- source for LuaSnip
         "rafamadriz/friendly-snippets", -- useful snippets
         "onsails/lspkind.nvim", -- vs-code like pictograms
     },
