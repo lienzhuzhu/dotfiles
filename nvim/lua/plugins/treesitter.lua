cd5327448c89c7a7367b535067c72fa4467da5a1
Lien Zhu <lienzhucs@gmail.com>
2023-12-20 06:38:44 -0800

    nvim: adds treesitter text objects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,53 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
 
-    config = function()
+        config = function()
 
-        require'nvim-treesitter.configs'.setup({
-            ensure_installed = { "c", "lua", "python", "vim", "vimdoc", "query" },
-            auto_install = true,
-            highlight = {
-                enable = true
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<leader>si",
-                    node_incremental = "<leader>sn",
-                    scope_incremental = "<leader>ss",
-                    node_decremental = "<leader>sd",
+            require'nvim-treesitter.configs'.setup({
+                ensure_installed = { "c", "lua", "python", "vim", "vimdoc", "query" },
+                auto_install = true,
+                highlight = {
+                    enable = true
                 },
-            },
-        })
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<leader>si",
+                        node_incremental = "<leader>sn",
+                        scope_incremental = "<leader>ss",
+                        node_decremental = "<leader>sd",
+                    },
+                },  
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
 
-    end
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ap"] = "@parameter.outer",
+                            ["ip"] = "@parameter.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v',
+                            ['@function.outer'] = 'V',
+                            ['@class.outer'] = '<c-v>',
+                        },
+                        include_surrounding_whitespace = true,
+                    },
+                },
+            })
+
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = "nvim-treesitter/nvim-treesitter"
+    }
 }
