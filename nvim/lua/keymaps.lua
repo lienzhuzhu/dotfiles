c094ca1357f60ff462d0728cc8bb7aaaea7c13ce
Lien Zhu <lienzhucs@gmail.com>
2023-12-20 10:36:05 -0800

    nvim: adds keymap for closing current buffer (aka tab)

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,4 +1,8 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+
+vim.keymap.set("n", "<leader>bc", "<cmd>bd<CR>", opts)
