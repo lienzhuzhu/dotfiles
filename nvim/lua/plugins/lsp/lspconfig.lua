17cecd002332c631843d2f76dcd189f2a6ea893c
Lien Zhu <lienzhucs@gmail.com>
2023-12-20 20:31:30 -0800

    nvim: adds telescope ui select extension

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -22,37 +22,37 @@ return {
             keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
             opts.desc = "Go to declaration"
-            keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+            keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
             opts.desc = "Show LSP definitions"
-            keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+            keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
             opts.desc = "Show LSP implementations"
-            keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+            keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
             opts.desc = "Show LSP type definitions"
-            keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+            keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
             opts.desc = "See available code actions"
-            keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+            keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
             opts.desc = "Smart rename"
-            keymap.set("n", "gr", vim.lsp.buf.rename, opts) -- smart rename
+            keymap.set("n", "gr", vim.lsp.buf.rename, opts)
 
             opts.desc = "Show buffer diagnostics"
-            keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+            keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
             opts.desc = "Show line diagnostics"
-            keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+            keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
             opts.desc = "Go to previous diagnostic"
-            keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+            keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
             opts.desc = "Go to next diagnostic"
-            keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+            keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
             opts.desc = "Show documentation for what is under cursor"
-            keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+            keymap.set("n", "K", vim.lsp.buf.hover, opts)
         end
 
         local capabilities = cmp_nvim_lsp.default_capabilities()
@@ -62,7 +62,6 @@ return {
             on_attach = on_attach,
         })
 
-        -- configure css server
         lspconfig["cssls"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
@@ -78,12 +77,10 @@ return {
             on_attach = on_attach,
             settings = {
                 Lua = {
-                    -- make the language server recognize "vim" global
                     diagnostics = {
                         globals = { "vim" }
                     },
                     workspace = {
-                        -- make language server aware of runtime files
                         library = {
                             [vim.fn.expand("$VIMRUNTIME/lua")] = true,
                             [vim.fn.stdpath("config") .. "/lua"] = true,
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,15 +1,30 @@
 return {
-    "nvim-telescope/telescope.nvim", tag = "0.1.2",
-    dependencies = { {"nvim-lua/plenary.nvim"} },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' },
 
-    config = function()
+        config = function()
 
-        local builtin = require("telescope.builtin")
+            require("telescope").setup({
+                extensions = {
+                    ['ui-select'] = {
+                        require("telescope.themes").get_dropdown {
+                        }
+                    }
+                }
+            })
 
-        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>pr", builtin.git_files, {})
-        vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>pg", builtin.grep_string, {})
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>pr", builtin.git_files, {})
+            vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>pg", builtin.grep_string, {})
 
-    end
+            require("telescope").load_extension("ui-select")
+
+        end
+    }
 }
