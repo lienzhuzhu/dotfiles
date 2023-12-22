9792a013542a7c3133876fa93b933c620d27037d
Lien Zhu <lienzhucs@gmail.com>
2023-12-21 23:07:50 -0800

    nvim: added some nice key maps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
 vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
 
 vim.keymap.set("n", "<leader>bc", "<cmd>bd<CR>", opts)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -8,7 +8,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.autoindent = true
+--vim.opt.autoindent = true
 vim.opt.smartindent = true
 
 vim.opt.virtualedit = "block"
