238fa976299032e93ee8036dccd76778dd4d2d8d
Lien Zhu <lienzhucs@gmail.com>
2023-12-22 08:24:48 -0800

    wterm: set initial cols and rows

--- wezterm/window.lua
+++ wezterm/window.lua
@@ -17,6 +17,10 @@ function M.apply_to_config(config)
 
     config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
     config.enable_tab_bar = false
+    config.window_background_opacity = 0.96
+    config.macos_window_background_blur = 0
+    config.initial_cols = 170
+    config.initial_rows = 48
 
 end
 
