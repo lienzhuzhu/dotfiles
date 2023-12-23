df6652bec91fa73e2d500cd6405cc1e68bef1b2f
Lien Zhu <lienzhucs@gmail.com>
2023-12-23 00:48:54 -0800

    nvim: tab confirms first completion

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -31,37 +31,40 @@ return {
                 ["<C-d>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.config.disable,
                 ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-                ["<Tab>"] = cmp.mapping(
-                    function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-                        elseif luasnip.expandable() then
-                            luasnip.expand()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        elseif check_backspace() then
-                            fallback()
-                        else
-                            fallback()
-                        end
-                    end,
-                    { "i", "s", }
-                ),
+                ["<Tab>"] = cmp.mapping.confirm({ select = true }),
 
-                ["<S-Tab>"] = cmp.mapping(
-                    function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end,
-                    { "i", "s", }
-                ),
+                --["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+                --["<Tab>"] = cmp.mapping(
+                --    function(fallback)
+                --        if cmp.visible() then
+                --            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+                --        elseif luasnip.expandable() then
+                --            luasnip.expand()
+                --        elseif luasnip.expand_or_jumpable() then
+                --            luasnip.expand_or_jump()
+                --        elseif check_backspace() then
+                --            fallback()
+                --        else
+                --            fallback()
+                --        end
+                --    end,
+                --    { "i", "s", }
+                --),
+
+                --["<S-Tab>"] = cmp.mapping(
+                --    function(fallback)
+                --        if cmp.visible() then
+                --            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+                --        elseif luasnip.jumpable(-1) then
+                --            luasnip.jump(-1)
+                --        else
+                --            fallback()
+                --        end
+                --    end,
+                --    { "i", "s", }
+                --),
 
             }),
             sources = cmp.config.sources({
