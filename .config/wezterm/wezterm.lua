169d1c52b8ed0b632f0e01019cd4ff820a329915
Lien Zhu <lienzhucs@gmail.com>
2023-12-24 04:49:22 -0800

    wezterm: found kanagawa colorscheme, wip refactor to match alacritty

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = {}
 
 -- Window settings
 --
-config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
+config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 config.window_background_opacity = 0.96
 config.macos_window_background_blur = 0
@@ -32,8 +32,11 @@ config.keys = {
 
 -- Color schemes and visual settings
 --
-config.color_scheme = "Batman"
-config.window_background_opacity = 0.94
+--config.color_scheme = "kanagawa (Gogh)"
+config.color_scheme = "kanagawabones"
+config.colors = {
+     background = "#181616"
+}
 
 
 -- Font settings
