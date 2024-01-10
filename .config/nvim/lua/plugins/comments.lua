56e401fba01a98c12483fc1992ae860e36893418
Lien Zhu <lienzhucs@gmail.com>
2024-01-10 00:04:42 -0800

    nvim: adds Trouble to spec

--- .config/nvim/lua/plugins/comments.lua
+++ .config/nvim/lua/plugins/comments.lua
@@ -1,6 +1,19 @@
 return {
     "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        {
+            "nvim-lua/plenary.nvim"
+        },
+        {
+            "folke/trouble.nvim",
+            dependencies = { "nvim-tree/nvim-web-devicons" },
+            opts = {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            },
+        }
+    },
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
