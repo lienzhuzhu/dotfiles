6b615b7763cab0452b7d36b713fb5588eada5613
Lien Zhu <lienzhucs@gmail.com>
2024-01-12 12:38:04 -0800

    nvim: cleans up keymappings

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -8,7 +8,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
 vim.keymap.set("n", "<leader>bc", "<cmd>bd<CR>", opts)
 
 -- for some reason these must use the ':' character and not <cmd>
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
-vim.keymap.set("i", "<C-m>", "<nop>", {})
+vim.keymap.set("i", "<C-m>", "<nop>", opts) -- NOTE: this mapping clears the default behavior so that nvim-cmp can use it to open the completion menu
