5851558cd6e248189357c4fa5c20589b05250345
Lien Zhu <lienzhucs@gmail.com>
2024-01-11 21:14:16 -0800

    nvim: wip neotree comparison

---
+++ .config/nvim/lua/unused/neotree.lua
@@ -0,0 +1,23 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+        "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+
+    config = function ()
+
+        require("neo-tree").setup({
+            close_if_last_window = true,
+            filesystem = {
+                hijack_netrw_behavior = "open_current",
+            }
+        })
+
+        vim.keymap.set("n", "<leader>t", "<cmd>Neotree toggle<CR>", { silent = true })
+
+    end
+}
