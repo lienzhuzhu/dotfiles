81b4afa85b9d7566006c8d4b2d68a99637ae892a
Lien Zhu <lienzhucs@gmail.com>
2024-12-08 19:37:17 -0600

    fixed prompt

--- .config/zsh/prompt.sh
+++ .config/zsh/prompt.sh
@@ -17,8 +17,8 @@ autoload -Uz vcs_info
 precmd() { vcs_info }
 
 # Format the vcs_info_msg_0_ variable
-zstyle ':vcs_info:git:*' formats '%b'
+zstyle ':vcs_info:git:*' formats ' on %b'
 zstyle ':vcs_info:*' enable git
 
 setopt PROMPT_SUBST
-PROMPT=$'%B%F{red}YT:AcademicCrashout%f%b in %F{white}%~%f on %B%F{red}${vcs_info_msg_0_}%f%b %B%F{blue}>%f%b '
+PROMPT=$'%B%F{red}YT:AcademicCrashout%f%b in %F{white}%~%f%B%F{red}${vcs_info_msg_0_}%f%b %B%F{blue}>%f%b '
