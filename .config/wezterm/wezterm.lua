31c15637029c4ec19ace5d28f839cd6ebc5d9120
Lien Zhu <lienzhucs@gmail.com>
2024-12-13 17:18:39 -0600

    wezterm mods

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,8 +12,8 @@ config.audible_bell = "Disabled"
 --
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
-config.window_background_opacity = 0.94
-config.macos_window_background_blur = 0
+config.window_background_opacity = 0.80
+config.macos_window_background_blur = 36
 config.initial_cols = 170
 config.initial_rows = 48
 config.window_close_confirmation = "NeverPrompt"
