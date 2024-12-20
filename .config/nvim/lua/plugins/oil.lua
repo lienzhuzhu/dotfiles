223f102adf25b1995ead3542f41129a693801216
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2024-12-19 21:21:29 -0600

    nvim: adds oil keybinding

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,5 +1,9 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    init = function ()
+        local opts = { noremap = true, silent = true }
+        vim.keymap.set("n", "<leader>t", "<cmd>Oil<CR>", opts)
+    end
 }
