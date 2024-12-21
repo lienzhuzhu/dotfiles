d1fbfde0903b87c516ca6dac4896cb8b2a58c6cb
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2024-12-20 21:48:10 -0600

    cleaner

--- config-mac/zsh/env.sh
+++ config-mac/zsh/env.sh
@@ -1,10 +1,11 @@
 export VISUAL=nvim
 export EDITOR=nvim
-export PASSWORD_STORE_DIR="$XDG_DATA_HOME/password-store"
+export PASSWORD_STORE_DIR="${XDG_DATA_HOME}/password-store"
 
 export HISTSIZE=10000                               # Maximum events for internal history
 export SAVEHIST=10000                               # Maximum events in history file
+export HISTFILE="${XDG_CACHE_HOME}/.zsh_history"
 
 ## CACHE Paths ##
-export YARN_CACHE_FOLDER="$XDG_CACHE_HOME/yarn"
-export npm_config_cache="$XDG_CACHE_HOME/npm"
+export YARN_CACHE_FOLDER="${XDG_CACHE_HOME}/yarn"
+export npm_config_cache="${XDG_CACHE_HOME}/npm"
