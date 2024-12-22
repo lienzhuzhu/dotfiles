af505fc6bdb81ab6cdb45731e2c8380d4ea9d714
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2024-12-21 22:37:17 -0600

    adds markdown, rmd autocmd for indentation

--- config-mac/nvim/lua/config/autocmds.lua
+++ config-mac/nvim/lua/config/autocmds.lua
@@ -1,1 +1,18 @@
--- This file sets various autocmds
+-- config/autocmds.lua
+-- Author: Lien Zhu
+-- Description: This file sets various autocmds
+
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "markdown", "rmd" },
+    callback = function()
+        -- Clear custom indentation and formatting expressions
+        vim.opt_local.indentexpr = ""
+        vim.opt_local.formatexpr = ""
+
+        -- Optionally, reset related options
+        vim.opt_local.autoindent = false
+        vim.opt_local.smartindent = false
+        vim.opt_local.formatoptions = "" -- Remove all formatting options
+    end,
+})
