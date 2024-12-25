dbaf466f0f0c4e46df71dce6a358abefd5979e50
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2024-12-25 17:46:02 -0600

    prompt

--- config-mac/zsh/prompt.sh
+++ config-mac/zsh/prompt.sh
@@ -26,4 +26,4 @@ check_unstaged_changes() {
 zstyle ':vcs_info:*' enable git
 
 setopt PROMPT_SUBST
-PROMPT=$'%B%F{red}YT:AcademicCrashout%f%b in %F{white}%~%f${vcs_info_msg_0_} %B%F{blue}>%f%b '
+PROMPT=$'%B%F{red}YT:AcademicCrashout%f%b in %F{yellow}%~%f${vcs_info_msg_0_} %B%F{blue}>%f%b '
