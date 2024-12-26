613f0dcf309ad7e85144cbcef4cf04bd12b5addb
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2024-12-25 18:47:59 -0600

    vimtex

--- config-mac/nvim/lua/plugins/latex.lua
+++ config-mac/nvim/lua/plugins/latex.lua
@@ -4,7 +4,7 @@ return {
     init = function()
 
         vim.g.tex_flavor = "latex"
-        vim.g.vimtex_quickfix_mode = 0
+        vim.g.vimtex_quickfix_mode = 1
 
         vim.g.vimtex_view_method = "sioyek"
 
