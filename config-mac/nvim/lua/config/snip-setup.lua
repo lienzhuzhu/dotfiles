d53bf4ff16222fede781d4ffeb35ac834521e1ab
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2024-12-27 01:53:32 -0600

    autosnippets

--- config-mac/nvim/lua/config/snip-setup.lua
+++ config-mac/nvim/lua/config/snip-setup.lua
@@ -1,3 +1,7 @@
+require("luasnip").config.set_config({
+    enable_autosnippets = true, -- Enable auto-snippets globally
+})
+
 require("luasnip.loaders.from_lua").load({
     paths = (os.getenv("XDG_CONFIG_HOME") or "~/.config/") .. "/nvim/luasnippets"
 })
--- config-mac/nvim/luasnippets/markdown.lua
+++ config-mac/nvim/luasnippets/markdown.lua
@@ -3,5 +3,17 @@ local s = ls.snippet
 local t = ls.text_node
 
 return {
-	s("hw", t("hello world of luasnip"))
+	s("hw", t("hello world of luasnip")),
+    s(
+    { -- Table 1: snippet parameters
+      trig="sayhi",
+      dscr="An autotriggering snippet that expands 'hi' into 'Hello, world!'",
+      regTrig=false,
+      priority=1000,
+      snippetType="autosnippet"
+    },
+    {
+      t("Hello, world!"), -- A single text node
+    }
+  )
 }
