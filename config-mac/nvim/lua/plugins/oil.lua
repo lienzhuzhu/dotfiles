0e1299e498067b08077837c063d43ed2bdd8ddc1
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2024-12-28 18:10:33 -0600

    oil

--- config-mac/nvim/lua/plugins/oil.lua
+++ config-mac/nvim/lua/plugins/oil.lua
@@ -5,6 +5,20 @@ return {
         local opts = { noremap = true, silent = true }
         vim.keymap.set("n", "<leader>t", "<cmd>Oil<CR>", opts)
 
-        return {}
+        return {
+            view_options = {
+                is_always_hidden = function(name, _)
+                    return
+                        vim.endswith(name, ".pdf") or
+                        vim.endswith(name, ".aux") or
+                        vim.endswith(name, ".out") or
+                        vim.endswith(name, ".fls") or
+                        vim.endswith(name, ".synctex.gz") or
+                        vim.endswith(name, ".toc") or
+                        vim.endswith(name, ".fdb_latexmk") or
+                        vim.endswith(name, ".git")
+                end,
+            }
+        }
     end
 }
