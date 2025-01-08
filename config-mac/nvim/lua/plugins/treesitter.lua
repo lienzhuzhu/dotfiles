fc5dd3c0af5e05b35aed5c901d3420f0fcbc4cc3
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2025-01-08 13:53:17 -0600

    lazy

--- config-mac/nvim/lua/plugins/treesitter.lua
+++ config-mac/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         "nvim-treesitter/nvim-treesitter-textobjects"
     },
     opts = {
-        ensure_installed = { "markdown", "c", "lua", "python", "vim", "vimdoc", "query" },
+        ensure_installed = { "r", "markdown", "c", "lua", "python", "vim", "vimdoc", "query" },
         auto_install = true,
         highlight = {
             enable = true
