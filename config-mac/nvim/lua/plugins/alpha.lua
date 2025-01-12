3dc3f939161793a4b06c0062a919574dbbf17803
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2025-01-11 20:17:43 -0600

    alpha

---
+++ config-mac/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,33 @@
+return {
+    "goolord/alpha-nvim",
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+        local button = dashboard.button
+        local section = dashboard.section
+        section.header.val = {
+            [[                                                                       ]],
+            [[  ██████   █████                   █████   █████  ███                  ]],
+            [[ ░░██████ ░░███                   ░░███   ░░███  ░░░                   ]],
+            [[  ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████   ]],
+            [[  ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███  ]],
+            [[  ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███  ]],
+            [[  ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███  ]],
+            [[  █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████ ]],
+            [[ ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░  ]],
+            [[                                                                       ]],
+        }
+
+        section.buttons.val = {
+            button("f", "  Find file",
+            ":Telescope find_files hidden=true no_ignore=true<CR>"),
+            button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            button("c", "  Configuration",
+            ":e ~/.config/nvim/init.lua <CR>"),
+            button("u", "  Update plugins", ":Lazy sync<CR>"),
+            button("q", "󰈆  Quit", ":qa<CR>")
+        }
+        section.footer.val = require 'alpha.fortune'()
+        alpha.setup(dashboard.opts)
+    end
+}
--- config-mac/wezterm/wezterm.lua
+++ config-mac/wezterm/wezterm.lua
@@ -63,7 +63,7 @@ config.color_scheme = "OneHalfDark"
 
 -- Font settings
 --
-config.font_size = 21.0
+config.font_size = 20.0
 --config.font = wezterm.font("SauceCodePro Nerd Font")
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
