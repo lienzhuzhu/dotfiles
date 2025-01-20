7385222cca135c888217d7ef0b246bbed4492e32
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2025-01-19 20:30:13 -0600

    snippet for front matter

--- config-mac/nvim/luasnippets/markdown.lua
+++ config-mac/nvim/luasnippets/markdown.lua
@@ -15,5 +15,17 @@ return {
         t({"", "<br>", "", ""}),
         i(0),
     }),
-    s("!", t({"> [!IMPORTANT]", "> "}), i(0)),
+    s("!", {
+        t({"> [!IMPORTANT]", "> "}),
+        i(0)
+    }),
+    s("fm", {
+        t({ "+++", "" }),
+        t("title = \""), i(1, "Page Title"), t({"\"", ""}),
+        t("description = \""), i(2, "Short description"), t({"\"", ""}),
+        t("date = \""), i(3, os.date("%Y-%m-%d")), t({"\"", ""}),
+        t("draft = "), c(4, { t("false"), t("true") }), t({ "", "" }),
+        t({ "+++", "", "" }),
+        i(0),
+    })
 }
