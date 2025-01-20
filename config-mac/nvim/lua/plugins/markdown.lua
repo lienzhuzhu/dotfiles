4192b3f63f6b5ac8f4db5c1875013c7c9a0fca5c
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2025-01-20 13:02:17 -0600

    markdown preview

--- config-mac/nvim/lua/plugins/markdown.lua
+++ config-mac/nvim/lua/plugins/markdown.lua
@@ -1,6 +1,6 @@
 return {
     {
-        'MeanderingProgrammer/render-markdown.nvim',
+        'MeanderingProgrammer/render-markdown.nvim', -- Preview within editor
         enabled = true,
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
         opts = {
@@ -11,8 +11,21 @@ return {
         },
     },
     {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+        config = function()
+            vim.keymap.set("n", "<leader>md", "<cmd>MarkdownPreviewToggle<CR>")
+            vim.g.mkdp_markdown_css = ""
+        end
+    },
+    {
         "lienzhuzhu/r2pdf.nvim",
-        enabled = true,
+        enabled = false,
         dev = true,
         init = function ()
             local opts = { noremap = true, silent = true }
