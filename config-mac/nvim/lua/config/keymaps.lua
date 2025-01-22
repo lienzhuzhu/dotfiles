a97a8f453ca0463f875ef592703c97504c6553bd
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2025-01-21 22:54:01 -0600

    some nice keymaps for searching

--- config-mac/nvim/lua/config/keymaps.lua
+++ config-mac/nvim/lua/config/keymaps.lua
@@ -47,7 +47,8 @@ end, opts)
 
 
 -- Open fzf-lua file picker
-vim.keymap.set("n", "<leader>pf", "<CMD>FzfLua files<CR>", opts)
+vim.keymap.set("n", "<leader>pf", "<CMD>FzfLua files<CR>", opts) -- "project fzf"
+vim.keymap.set("n", "<leader>pg", "<CMD>FzfLua live_grep<CR>", opts) -- "project grep"
 
 -- grep in current working directory with short PWD in prompt
 vim.keymap.set('n', 're', function()
@@ -61,12 +62,41 @@ vim.keymap.set('n', 're', function()
     end
 end, opts)
 
+-- grep word under cursor in pwd
+vim.keymap.set('n', '<leader>vg', ":lua vim.cmd('vimgrep /' .. vim.fn.expand('<cword>') .. '/g **/*')<CR>:copen<CR>", opts)
+
+-- grep selection in pwd
+-- vim.api.nvim_set_keymap('v', '<leader>vg', '"zy:lua vim.cmd(\'vimgrep /\' .. vim.fn.escape(vim.fn.getreg("z"), "/") .. \'/g **/*\')<CR>:copen<CR>', opts)
+vim.keymap.set('v', '<leader>vg', function()
+    -- Get the visually selected text
+    vim.cmd('noau normal! "vy')  -- Copy selection to register v without triggering autocommands
+    local selected_text = vim.fn.escape(vim.fn.getreg('v'), '/\\')  -- Get and escape the text
+
+    -- Run vimgrep with the selected text and open quickfix
+    vim.cmd('vimgrep /' .. selected_text .. '/g **/*')
+    vim.cmd('copen')
+end, opts)
+
+
 -- grep in NOTAKER_CORTEX_DIR
 vim.keymap.set('n', '<leader>ng', function()
-    local search_term = vim.fn.input("NOTAKER grep: ")
+    local search_term = vim.fn.input("Ngrep: ")
     if search_term ~= "" then
         local cmd = string.format('vimgrep /%s/g $NOTAKER_CORTEX_DIR/**/*.md', search_term)
         vim.cmd(cmd)
         vim.cmd('copen')
     end
+end, opts)
+
+-- necrotizing fasciitis
+vim.keymap.set('n', '<leader>NG', ":lua vim.cmd('vimgrep /' .. vim.fn.expand('<cword>') .. '/g $NOTAKER_CORTEX_DIR/**/*')<CR>:copen<CR>", opts)
+-- vim.api.nvim_set_keymap('v', '<leader>NG', '"zy:lua vim.cmd(\'vimgrep /\' .. vim.fn.escape(vim.fn.getreg("z"), "/") .. \'/g $NOTAKER_CORTEX_DIR/**/*\')<CR>:copen<CR>', opts)
+vim.keymap.set('v', '<leader>NG', function()
+    -- Get the visually selected text
+    vim.cmd('noau normal! "vy')  -- Copy selection to register v without triggering autocommands
+    local selected_text = vim.fn.escape(vim.fn.getreg('v'), '/\\')  -- Get and escape the text
+
+    -- Run vimgrep with the selected text and open quickfix
+    vim.cmd('vimgrep /' .. selected_text .. '/g $NOTAKER_CORTEX_DIR/**/*')
+    vim.cmd('copen')
 end, opts)
