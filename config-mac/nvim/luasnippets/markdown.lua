3701964b7320df18787da8cb4371798c5231481b
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2025-01-23 00:44:30 -0600

    adds Note snippet

--- config-mac/nvim/luasnippets/markdown.lua
+++ config-mac/nvim/luasnippets/markdown.lua
@@ -2,19 +2,12 @@
 -- Description: Snippets exclusive for Markdown and R Markdown files.
 
 
+local function filename_no_ext()
+    return vim.fn.expand("%:t:r"):gsub("_", " ")
+end
+
+
 return {
-	s("hw", t("hello world of luasnip")),
-    s({
-        trig="vb",
-        dscr="Inserts a vertical break",
-        regTrig=false,
-        priority=1000,
-        snippetType="autosnippet"
-    },
-    {
-        t({"", "<br>", "", ""}),
-        i(0),
-    }),
     s("!", {
         t({"> [!IMPORTANT]", "> "}),
         i(0)
@@ -26,6 +19,13 @@ return {
         t("date = \""), i(3, os.date("%Y-%m-%d")), t({"\"", ""}),
         t("draft = "), c(4, { t("false"), t("true") }), t({ "", "" }),
         t({ "+++", "", "" }),
+        i(0),
+    }),
+    s("nt", {
+        t("# "), f(filename_no_ext, {}), t({"", ""}),
+        t("> Date: "), i(1, os.date("%Y-%m-%d")), t({"", "", ""}),
+        t("> Instructor: "), i(2), t({"", "", ""}),
+        t("## "), i(3), t({"", "", ""}),
         i(0),
     })
 }
